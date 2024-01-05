711d24d3afd9c0c878327766bd4967a703ca12ef
donatello30 <152093214+donatello30@users.noreply.github.com>
2024-01-05 17:02:33 -0500

    Update biartix.sh

--- biartix.sh
+++ biartix.sh
@@ -15,7 +15,7 @@ echo root:12345 | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
  
-pacman -S grub efibootmgr networkmanager networkmanager-runit nmtui dialog mtools dosfstools linux-headers avahi xdg-user-dirs xdg-utils inetutils dnsutils bluez bluez-utils hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid terminus-font
+pacman -S grub efibootmgr networkmanager networkmanager-runit dialog mtools dosfstools linux-headers avahi xdg-user-dirs xdg-utils inetutils dnsutils bluez bluez-utils hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
