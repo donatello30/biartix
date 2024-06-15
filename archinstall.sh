f5b4acd81f1eb0a4b033085f36ed4fae67989dad
donatello30 <152093214+donatello30@users.noreply.github.com>
2024-06-15 14:10:52 -0500

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -35,7 +35,7 @@ systemctl enable NetworkManager
 #systemctl enable sshd
 #systemctl enable avahi-daemon
 #systemctl enable tlp # You can comment this command out if you didn't install tlp, see #above
-systemctl enable reflector.timer
+#systemctl enable reflector.timer
 #systemctl enable fstrim.timer
 #systemctl enable libvirtd
 #systemctl enable firewalld
