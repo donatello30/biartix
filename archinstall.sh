eeb9b0b449c59cb126f5b68f1882e6e3b1389958
donatello30 <152093214+donatello30@users.noreply.github.com>
2024-06-15 14:09:15 -0500

    Add files via upload

---
+++ archinstall.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Todo lo que sigue es despues de haber hecho el pacstrap /mnt base base-devel
+# Y el arch-chroot /mnt
+# this is for  "LANG=en_US.UTF-8":   sed -i '177s/.//' /etc/locale.gen
+ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
+hwclock --systohc
+sed -i '185s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=es_CO.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=la-latin1" >> /etc/vconsole.conf
+echo "redakuma" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 redakuma.localdomain redakuma" >> /etc/hosts
+echo root:123 | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+ 
+pacman -S grub os-prober efibootmgr networkmanager network-manager-applet linux-headers dosfstools xdg-user-dirs xdg-utils intel-ucode acpid reflector
+
+#pacman -S xf86_64-video-intel xorg --ignore xorg-server-xdmx sddm sddm-runit
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB 
+#change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+#systemctl enable bluetooth
+#systemctl enable cups.service
+#systemctl enable sshd
+#systemctl enable avahi-daemon
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see #above
+systemctl enable reflector.timer
+#systemctl enable fstrim.timer
+#systemctl enable libvirtd
+#systemctl enable firewalld
+systemctl enable acpid
+
+# Create User
+useradd -m user
+echo user:123 | chpasswd
+usermod -aG wheel user
+echo "user ALL=(ALL) ALL" >> /etc/sudoers.d/user
+
+printf "\e[1;32mLo lograste! crea el usuario y password despues Type exit, umount -R /mnt and reboot.\e[0m"
+
+
+# Como utilizar reflector:$  reflector --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
